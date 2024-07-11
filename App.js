0b0e59e98d939fee003d66e61be6d060c4c2df3a
kizuyoko <kizuyoko@gmail.com>
2024-07-11 15:01:41 +0200

    Screen Headers styling

--- App.js
+++ App.js
@@ -10,12 +10,21 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-      <StatusBar style='dark' />
+      <StatusBar style='light' />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator 
+          screenOptions={{
+            headerStyle: { backgroundColor: '#351401' },
+            headerTintColor: 'white',
+            contentStyle: { backgroundColor: '#3f2f25' }
+          }}
+        >
           <Stack.Screen 
             name="MealsCategories"
             component={CategoriesScreen}
+            options={{
+              title: 'All Categories'
+            }}
           />
           <Stack.Screen 
             name="MealsOverview"
